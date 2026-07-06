fa5484dfdbef6726ee70ab5e7ac096e255b98199
VRPioneerPaul <paul100burger@gmail.com>
2026-07-07 00:09:19 +0200

    Removed unused files

--- app/api/routes/discord.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/discord", tags=["discord"])
-
-@router.get("/status")
-async def status():
-    return {"status": "offline", "version": "1.0.0"}
